e062c353f04acaa64dae2e2a3ed83cb797bc4f1b
kadirbelen <61799635+kadirbelen@users.noreply.github.com>
2021-02-16 01:26:18 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal  _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                Console.WriteLine("Araba eklendi.");
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba eklenemedi. Araba ismi minimum 2 karakter olmalıdır ve Araba günlük fiyatı 0'dan büyük olmalıdır.");
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetAllByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+    }
+}
