ab09a18afd7baf5ead8ecc31e4290905393c4b28
HAKKI EYİLİK <eyilik_1987@hotmail.com>
2021-04-10 16:49:58 +0300

    duzeltme

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp
             //RentalAdd();
             //UserTest();
             //CustomerTest();
+            Console.WriteLine("Mesaj");
         }
 
         private static void CustomerTest()
